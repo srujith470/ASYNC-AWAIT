f6c67e143929bd713c307ad35bdcac3f8fa9a2f4
srujith470 <srujith.reddy2012@gmail.com>
2019-01-30 00:20:21 -0500

    ASYNC AWAIT

---
+++ app-promises.js
@@ -0,0 +1,90 @@
+const users =[{
+    id:1,
+    name:'andrew',
+    schoolID:101
+},{
+    id:2,
+    name:'andrew2',
+    schoolID:102
+},{
+    id:3,
+    name:'andrew3',
+    schoolID:103
+},{
+    id:4,
+    name:'andrew4',
+    schoolID:104
+}];
+const grades = [{
+    id:2,
+    grade:88,
+    schoolID:102
+},{
+    id:1,
+    grade:98,
+    schoolID:101
+},{
+    id:3,
+    grade:84,
+    schoolID:103
+},{
+    id:4,
+    grade:84,
+    schoolID:104
+}];
+
+const getUser=(id) => {
+    return new Promise((resolve,reject) => {
+        const user  = users.find((user) => {
+            return user.id === id
+        });
+        if(user){
+            resolve(user)
+        }else{
+            reject(`Unable to find user by id of ${id}`)
+        }
+    })
+}
+
+const getGrades=(schoolId) => {
+    return new Promise((resolve,reject) => {
+        resolve(grades.filter((grade)=> grade.schoolID == schoolId  ));
+    })
+};
+
+const getStatus =(userID) => {
+    let user
+    return getUser(userID).then((tempuser)=> {
+    user =tempuser    
+    return getGrades(user.schoolID);
+    }).then((grades) =>{
+        let average = 0
+        if(grades.length>0){
+            average = grades.map((grade) =>grade.grade).reduce((a,b) => a+b)/grades.length
+        }
+        console.log(average);
+        return `${user.name} has a ${average}% in class`
+    })
+}
+
+const getStatusAlt = async (userId) => {
+    return 'Mike';
+}
+console.log(getStatusAlt())
+
+// getUser(1).then((user) =>{
+//     console.log(user)
+// }).catch((e) => {
+// console.log(e)
+// })
+getStatus(2).then((user) =>{
+    console.log(user)
+}).catch((e) => {
+console.log(e)
+})
+
+// getGrades(104).then((user) =>{
+//     console.log(user)
+// }).catch((e) => {
+// console.log(e)
+// })
